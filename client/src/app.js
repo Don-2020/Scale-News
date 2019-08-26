609d00a37a810a50fc0563e469cc680c6e86b02f
Donirvin Dickerson <Dond2018@gmail.com>
2019-08-26 13:56:35 -0700

    Fixing

--- client/src/app.js
+++ client/src/app.js
@@ -18,7 +18,23 @@ import Carousel from 'react-bootstrap/Carousel'
 // import TopicPage from '../src/pages/TopicPage'
 // <TopicPage topics={this.state.topicsArray}/>
 // ========================================================
-
+proxy = () => {
+  var cors_api_host = 'https://cors-anywhere.herokuapp.com/';
+  var cors_api_url = cors_api_host + '/';
+  var slice = [].slice;
+  var origin = window.location.protocol + '//' + window.location.host;
+  var open = XMLHttpRequest.prototype.open;
+  XMLHttpRequest.prototype.open = function () {
+    var args = slice.call(arguments);
+    var targetOrigin = /^https?:\/\/([^\/]+)/i.exec(args[1]);
+    if (targetOrigin && targetOrigin[0].toLowerCase() !== origin &&
+      targetOrigin[1] !== cors_api_host) {
+      args[1] = cors_api_url + args[1];
+    }
+    return open.apply(this, args);
+  };
+}
+proxy();
 export default class app extends React.Component {
   state = {
     showModal: false,
