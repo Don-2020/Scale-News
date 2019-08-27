f34098714f9f5282b64a741792761e160ae9f951
Meridith Berk <mberk8855@gmail.com>
2019-08-26 17:53:57 -0700

    final touches css

--- client/src/Components/Carousel/carousel.js
+++ client/src/Components/Carousel/carousel.js
@@ -5,12 +5,21 @@ import OpinionBtn from '../Button/OpinionBtn';
 import "./carousel.css";
 import axios from "axios";
 
+const CarouselCaptionCss={
+  backgroundColor: "rgba(255, 255, 255, 0.6)",
+  color: "#5f5f5f",
+  padding: "10px"
+}
+
 class ControlledCarousel extends Component {
 
     state = {
       news: []
     }
 
+     //axios is calling to the scrape/ the api scrape is executing the code and returning an array with all the news. 
+    //with this information we are updating the state
+   //the state then triggers the render 
   componentDidMount() {
     axios.get("api/scrape")
       .then(res => {
@@ -18,20 +27,23 @@ class ControlledCarousel extends Component {
       });
 }
 
+//mapping (for loop) the news and filling the carousel / grabbing position 1,2,3 for array and preparing carousel item with image article title and paragraph and url reference 
+
     render() {
       const { news } = this.state;
     return (
     <Carousel>
+
+      
       {news.map(article => {
         return <Carousel.Item>
-          <p></p>
           <a href={article.url} target="_blank">
           <img
             className="d-block w-100"
             src= {article.image}
             alt="First slide"
           /></a>
-          <Carousel.Caption>
+          <Carousel.Caption style={CarouselCaptionCss}>
             <h3>{article.title}</h3>
             <p>{article.paragraph}</p>
 
@@ -40,7 +52,8 @@ class ControlledCarousel extends Component {
             {article.other.map(opinion => {
 
               let style = {
-                backgroundColor: ''
+                backgroundColor: '',
+                color:"#ffffff"
               };
               
               if (opinion.pov === 'Right') {
